3a8d2f64efd715396d6700bbe6ba772153e9c38a
erikborges013 <erikborgesofficial@gmail.com>
2025-04-13 03:58:47 -0300

    upload projeto musical 1.0

---
+++ c# projeto musical/Program.cs
@@ -0,0 +1,44 @@
+﻿string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+void ExibirTextoNaTela()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda.");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas.");
+    Console.WriteLine("Digite 3 para avaliar uma banda.");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda.");
+    Console.WriteLine("Digite -1 para sair.");
+
+    Console.Write("Digite a opção escolhida:");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoNumericaEscolhida = int.Parse(opcaoEscolhida);
+    
+    switch (opcaoNumericaEscolhida) 
+    {
+        case 1: Console.WriteLine("Você escolheu a opção: " + opcaoNumericaEscolhida);
+        break;
+        case 2: Console.WriteLine("Você escolheu a opção: " + opcaoNumericaEscolhida);
+        break;
+        case 3: Console.WriteLine("Você escolheu a opção: " + opcaoNumericaEscolhida);
+        break;
+        case 4: Console.WriteLine("Você escolheu a opção: " + opcaoNumericaEscolhida);
+        break;
+        case -1: Console.WriteLine("Bye Bye *");
+        break;
+        default : Console.WriteLine("Opção inválida!");
+        break;
+    }
+}
+
+ExibirTextoNaTela();
+ExibirOpcoesDoMenu();
